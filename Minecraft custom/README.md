bc5a4477636915ac335819a7494975bbc9b01ccb
maikel Middelbos <53620759+smikkelmikkel@users.noreply.github.com>
2022-12-03 18:33:20 +0100

    Update 03-12-2022
    Update doorgevoerd omtrent de geautomatiseerde minecraft install op paper!
    
    Updated by:
    Maikel. M || CEO

--- Minecraft custom/README.md
+++
@@ -1,4 +0,0 @@
-# pterodactylpanel
-Hey, leuk dat je komt kijken bij deze github!
-
-Dit is allemaal voor de gamehosting die ik momenteel heb [panel](https://game.flatnode.net)
--- bash/test.bash
+++ bash/test.bash
@@ -1,20 +1,3 @@
-echo "[Flatnode] Installatie is gestard!"
-sleep 5s
-apt install docker 
-systemctl enable --now docker
-mkdir -p /etc/pterodactyl
-curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
-chmod u+x /usr/local/bin/wings
-curl -o /etc/systemd/system/wings.service https://raw.githubusercontent.com/ZelixNode/wings/main/wings.service
+echo "Welkom bij je pterodactyl installatie!"
+echo "Wij hebben 2 opties voor u die u kunt kiezen!"
 
-
-echo "ssl domein"
-
-read ssldomein
-
-apt install nginx -y
-sudo apt update
-sudo apt install -y certbot
-sudo apt install -y python3-certbot-nginx
-
-certbot certonly --nginx -d $ssldomein
